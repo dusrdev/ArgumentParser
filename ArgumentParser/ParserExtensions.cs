b8a2a14cea8c37dba52d3ded3fc4e819877d6011
David Shnayder <dusrdev@outlook.com>
2022-12-18 21:43:54 +0200

    Added 2 more validation methods for string and ensuring key exists

--- ArgumentParser/ParserExtensions.cs
+++ ArgumentParser/ParserExtensions.cs
@@ -2,6 +2,51 @@
 
 public static class ParserExtensions {
     /// <summary>
+    /// Attempts to retrieve a value
+    /// </summary>
+    /// <param name="arguments"></param>
+    /// <param name="key"></param>
+    /// <param name="throwIfUnable">Throw an appropriate exception if unable to convert the value</param>
+    /// <exception cref="KeyNotFoundException">If the key was not found</exception>
+    /// <exception cref="ArgumentException">If the value was null or whitespace</exception>
+    /// <remarks>
+    /// <para>A null return value is only possible if <paramref name="throwIfUnable"/> is false</para>
+    /// <para>The <paramref name="key"/> will be added either to the message or as a property to the exceptions in order to maintain detail</para>
+    /// </remarks>
+    public static string? GetStringValue(this Dictionary<string, string> arguments, string key, bool throwIfUnable = false) {
+        return arguments.GetStringValue(key, throwIfUnable, Parser.ExceptionMessages);
+    }
+
+    /// <summary>
+    /// Attempts to retrieve a value
+    /// </summary>
+    /// <param name="arguments"></param>
+    /// <param name="key"></param>
+    /// <param name="throwIfUnable">Throw an appropriate exception if unable to convert the value</param>
+    /// <param name="messages">Adds an option to override the exception output message</param>
+    /// <exception cref="KeyNotFoundException">If the key was not found</exception>
+    /// <exception cref="ArgumentException">If the value was null or whitespace</exception>
+    /// <remarks>
+    /// <para>A null return value is only possible if <paramref name="throwIfUnable"/> is false</para>
+    /// <para>The <paramref name="key"/> will be added either to the message or as a property to the exceptions in order to maintain detail</para>
+    /// </remarks>
+    public static string? GetStringValue(this Dictionary<string, string> arguments, string key, bool throwIfUnable, ParserExceptionMessages messages) {
+        if (!arguments.TryGetValue(key, out string? value)) {
+            if (throwIfUnable) {
+                throw new KeyNotFoundException($"{messages.KeyNotFound} --> Key: \"{key}\"");
+            }
+            return null;
+        }
+        if (string.IsNullOrWhiteSpace(value)) {
+            if (throwIfUnable) {
+                throw new ArgumentException(messages.Null, key);
+            }
+            return null;
+        }
+        return value;
+    }
+
+    /// <summary>
     /// Attempts to convert a value to an integer
     /// </summary>
     /// <param name="arguments"></param>
@@ -152,5 +197,17 @@ public static class ParserExtensions {
             return null;
         }
         return result;
+    }
+
+    /// <summary>
+    /// Ensures that the argument exists, otherwise it throws an exception
+    /// </summary>
+    /// <param name="arguments"></param>
+    /// <param name="key"></param>
+    /// <exception cref="KeyNotFoundException"></exception>
+    public static void EnsureKeyExists(this Dictionary<string, string> arguments, string key) {
+        if (!arguments.ContainsKey(key)) {
+            throw new KeyNotFoundException($"Argument not found: \"{key}\", but is required");
+        }
     }
 }
