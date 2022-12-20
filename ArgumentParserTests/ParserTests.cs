e2fbf02668b392fe4f5ac1e53ff88e5125f8bf8a
David Shnayder <dusrdev@outlook.com>
2022-12-20 15:37:08 +0200

    Updated tests to reflect new functionality

--- ArgumentParserTests/ParserTests.cs
+++ ArgumentParserTests/ParserTests.cs
@@ -25,70 +25,58 @@ public class ParserTests {
 
     [TestMethod]
     public void ParseArgumentsTest_Empty() {
-        Assert.ThrowsException<ArgumentException>(() => Parser.ParseArguments(Parser.Split("")));
+        Assert.IsNull(Parser.ParseArguments(Parser.Split("")));
     }
 
     [TestMethod]
     public void ParseArgumentsTest_Quotes() {
         string input = "--names \"Tacitus Kilgore\"";
         var expected = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase) { { "names", "Tacitus Kilgore" } };
-        CollectionAssert.AreEqual(expected, Parser.ParseArguments(input));
+        CollectionAssert.AreEqual(expected, Parser.ParseArguments(input)!.GetInnerDictionary());
     }
 
     [TestMethod]
     public void ParseArgumentsTest_CommandAndParameterWithValues() {
         string input = "command -p value1 --param2 value2";
         var expected = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase) {
-            { "Command", "command" },
+            { "0", "command" },
             { "p", "value1" },
             { "param2", "value2" }
         };
-        CollectionAssert.AreEqual(expected, Parser.ParseArguments(input));
+        CollectionAssert.AreEqual(expected, Parser.ParseArguments(input)!.GetInnerDictionary());
     }
 
     [TestMethod]
     public void ParseArgumentsTest_CommandAndParameterWithoutValue() {
         string input = "command -p -f text.txt";
         var expected = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase) {
-            { "Command", "command" },
+            { "0", "command" },
             { "p", string.Empty },
             { "f", "text.txt" }
         };
-        CollectionAssert.AreEqual(expected, Parser.ParseArguments(input));
+        CollectionAssert.AreEqual(expected, Parser.ParseArguments(input)!.GetInnerDictionary());
     }
 
     [TestMethod]
     public void ParseArgumentsTest_GeneralWithIEnumerable() {
         var input = Parser.Split("command -p -f text.txt");
         var expected = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase) {
-            { "Command", "command" },
+            { "0", "command" },
             { "p", string.Empty },
             { "f", "text.txt" }
         };
-        CollectionAssert.AreEqual(expected, Parser.ParseArguments(input));
+        CollectionAssert.AreEqual(expected, Parser.ParseArguments(input)!.GetInnerDictionary());
     }
 
     [TestMethod]
     public void ParseArgumentsTest_AllInclusive() {
         var input = Parser.Split("command -p fast -f \"text.txt text2.txt\" -v");
         var expected = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase) {
-            { "Command", "command" },
+            { "0", "command" },
             { "p", "fast" },
             { "f", "text.txt text2.txt" },
             { "v", string.Empty }
         };
-        CollectionAssert.AreEqual(expected, Parser.ParseArguments(input));
-    }
-
-    [TestMethod]
-    public void ParseArgumentsTest_SameNameCommandAndParamter() {
-        var input = Parser.Split("command --command fast -f \"text.txt text2.txt\" -v");
-        var expected = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase) {
-            { "CommandKey", "command" },
-            { "command", "fast" },
-            { "f", "text.txt text2.txt" },
-            { "v", string.Empty }
-        };
-        CollectionAssert.AreEqual(expected, Parser.ParseArguments(input, "CommandKey"));
+        CollectionAssert.AreEqual(expected, Parser.ParseArguments(input)!.GetInnerDictionary());
     }
 }
